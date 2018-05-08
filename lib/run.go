1659b8f7c958fdd0c9717ee4cb1eece4a66dc318
alex <gongqiang@laoyuegou.com>
2018-05-08 21:17:09 +0800

    fix

--- lib/run.go
+++ lib/run.go
@@ -11,19 +11,17 @@ import (
 )
 
 func Run(router *gin.Engine, port string) {
-
     if err := router.Run(port); err != nil {
         time.Sleep(time.Second)
         log.Fatal("service fail")
         return
     }
-
     c := make(chan os.Signal)
     signal.Notify(c, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGQUIT)
     for s := range c {
         switch s {
         case syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGHUP, syscall.SIGQUIT:
-            log.Println("service end receive signal %v", s)
+            log.Printf("service end receive signal %v", s)
             return
         default:
             log.Println("other")
@@ -38,8 +36,9 @@ func Start() {
     flag.StringVar(&envp, "envp", "", "please input --envp=`you config path`")
     flag.Parse()
     if envp == "" {
-        panic("name is not set :add  --envp=`you config path`  after go run main ")
+        panic("config path is not set :add  --envp=`you config path`  after go run main ")
     }
     //初始化配置
     TotalDefine.InitConfig(envp)
+    return
 }
